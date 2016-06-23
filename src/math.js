99128496b1ae133e659f7b9cdb1eb3d59e34e382
Thomas J. Shavor <thomasshavor@gmail.com>
2016-06-23 11:39:48 -0500

    testing completed

---
+++ src/math.js
@@ -0,0 +1,33 @@
+// add()
+function add(num1, num2){
+	return num1+num2;
+}
+// subtract
+
+function subtract(num1, num2){
+	return num1- num2;
+}
+
+  // multiply()
+
+function multiply(num1, num2){
+	return num1*num2;
+} 
+
+// divide()
+
+function divide(num1, num2){
+	return num1/num2;
+}
+
+ // square()
+
+function square(num1){
+	return num1*num1;
+}
+
+// squareRoot()
+function squareRoot(num1){
+	return Math.sqrt(num1);
+}
+ 
